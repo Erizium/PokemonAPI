3828497db0ef2321135989b693dd26e7183d67db
Erizium <tobbe.osterlin@gmail.com>
2021-04-13 15:51:19 +0200

    Can now search multiple times in a row

--- pokemon.js
+++ pokemon.js
@@ -2,21 +2,16 @@
 let fetchButton = document.getElementById("fetchButton");
 let input = document.getElementById("input");
 let ability = document.getElementById("ability");
-
+let search = 0
 let url = "https://pokeapi.co/api/v2/pokemon/"
 
-fetchButton.addEventListener("click", async event => {
 
+fetchButton.addEventListener("click", async event => {
+    search = 1
     console.log("Fetch button pressed.")
     let inputString = input.value;
-    let pokemon = inputString
+    let pokemon = inputString.toLowerCase()
     console.log(pokemon)
-
-    try {
-       //remove currently seen abilities to show new ones.
-    } catch {
-
-    }
 
     let response = await fetch(url)
     let data = await (await fetch(url + pokemon)).json()
@@ -26,6 +21,7 @@ fetchButton.addEventListener("click", async event => {
     let pName = data.species.name
     document.getElementById("name").innerHTML = pName.charAt(0).toUpperCase() + pName.slice(1);
 
+    imgDiv.innerHTML = "";
     for(i = 0; i < data.types.length; i++) {
 
         let typeText = data.types[i].type.name + "Text"
@@ -34,10 +30,12 @@ fetchButton.addEventListener("click", async event => {
         typeImg.src = document.getElementById(typeText).src
         typeImg.style.height = "15px";
         typeImg.style.width = "50px";
-
+        typeImg.style.display = "block";
         imgDiv.appendChild(typeImg);
     }
 
+    ability.innerHTML = "";
+    
     for(i = 0; i < data.abilities.length; i++) {
         let aDiv = document.createElement("div")
         let aText = data.abilities[i].ability.name
@@ -57,27 +55,36 @@ fetchButton.addEventListener("click", async event => {
         aDiv.appendChild(aDivInfo)
     }
 
+
     for(i = 0; i < data.types.length; i++) {
 
         console.log("Dmg relations: ")
-        let dmgRelation = await (await fetch(data.types[i].type.url)).json()
+        var dmgRelation = await (await fetch(data.types[i].type.url)).json()
         console.log(dmgRelation)
+       
+        if(search === 1) {
+            doubleDmgFrom.innerHTML = "";
+            doubleDmgTo.innerHTML = "";
+            halfDmgFrom.innerHTML = "";
+            halfDmgTo.innerHTML = "";
+            search = 0
+        }
+       
+        for(j = 0; j < dmgRelation.damage_relations.double_damage_from.length; j++) {
 
-        
-        for(i = 0; i < dmgRelation.damage_relations.double_damage_from.length; i++) {
-
-            let relText1 = dmgRelation.damage_relations.double_damage_from[i].name;
+            let relText1 = dmgRelation.damage_relations.double_damage_from[j].name;
             let dDF = document.createElement("img");
             dDF.src = document.getElementById(relText1 + "Text").src;
             dDF.style.height = "15px";
             dDF.style.width = "50px";
             doubleDmgFrom.appendChild(dDF);
+            console.log(relText1)
         }
+
         
+        for(j = 0; j < dmgRelation.damage_relations.double_damage_to.length; j++) {
 
-        for(i = 0; i < dmgRelation.damage_relations.double_damage_to.length; i++) {
-
-            let relText2 = dmgRelation.damage_relations.double_damage_to[i].name;
+            let relText2 = dmgRelation.damage_relations.double_damage_to[j].name;
             console.log( "Double dmg to: " + relText2)
 
             let dDT = document.createElement("img");
@@ -87,15 +94,26 @@ fetchButton.addEventListener("click", async event => {
             doubleDmgTo.appendChild(dDT);
         }
 
-        for(i = 0; i < dmgRelation.damage_relations.half_damage_to.length; i++) {
+        
+        for(j = 0; j < dmgRelation.damage_relations.half_damage_from.length; j++) {
 
-            let relText3 = dmgRelation.damage_relations.half_damage_to[i].name;
+            let relText3 = dmgRelation.damage_relations.half_damage_from[j].name;
+            let hDF = document.createElement("img");
+            hDF.src = document.getElementById(relText3 + "Text").src
+            hDF.style.height = "15px";
+            hDF.style.width = "50px";
+            halfDmgFrom.appendChild(hDF)
+        }
+
+        
+        for(j = 0; j < dmgRelation.damage_relations.half_damage_to.length; j++) {
+
+            let relText4 = dmgRelation.damage_relations.half_damage_to[j].name;
             let hDT = document.createElement("img");
-            hDT.src = document.getElementById(relText3 + "Text").src;
+            hDT.src = document.getElementById(relText4 + "Text").src;
             hDT.style.height = "15px";
             hDT.style.width = "50px";
             halfDmgTo.appendChild(hDT);
-        } 
+        }
     }
-
 })
